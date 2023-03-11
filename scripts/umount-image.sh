bcccb43a142232553743666aaf14f2bb1d8d699d
Luis Weich <weichluis.sw@gmail.com>
2023-03-11 20:52:18 +0100

    umount-image check dir

--- scripts/umount-image.sh
+++ scripts/umount-image.sh
@@ -5,6 +5,11 @@ read IMAGE_NAME
 
 IMAGE_MOUNT_DIR=~/image/$IMAGE_NAME
 
+if [ ! -d $IMAGE_MOUNT_DIR ]; then
+    echo Dir does not exist
+    exit 1
+fi
+
 sudo umount $IMAGE_MOUNT_DIR
 sudo cryptsetup close $IMAGE_NAME
 
