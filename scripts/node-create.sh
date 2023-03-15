cb7fdbd467891b8ae917a8668650c1eb85dbb883
Luis Weich <weichluis.sw@gmail.com>
2023-03-15 22:17:28 +0100

    node-create.sh do not forward ssh port

--- scripts/node-create.sh
+++ scripts/node-create.sh
@@ -21,11 +21,6 @@ fi
 
 new_passwd=$(uuidgen)
 
-host_port="$node_nr"
-host_port+="0022"
-
-device_name="ssh$host_port"
-
 # create container
 
 lxc launch ubuntu:22.04 $node_name --storage image-server-pool
@@ -35,7 +30,6 @@ lxc exec $node_name -- sh -c "sed -i 's/PasswordAuthentication no/PasswordAuthen
 lxc exec $node_name -- sh -c "usermod -s /usr/sbin/nologin root && usermod -s /usr/sbin/nologin ubuntu"
 
 lxc config set $node_name security.nesting true
-lxc config device add $node_name $device_name proxy listen=tcp:0.0.0.0:$host_port connect=tcp:0.0.0.0:22
 
 lxc restart $node_name
 
