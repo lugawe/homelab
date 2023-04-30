b4c8b77222dac008e5a4b6dba88f080a116b0734
luwe <luis.weich.de@gmail.com>
2023-04-30 03:13:30 +0200

    node-create.sh update

--- scripts/node-create.sh
+++ scripts/node-create.sh
@@ -21,13 +21,14 @@ lxc launch ubuntu:22.04 $node_name --storage nodes-pool
 sleep 3
 
 lxc exec $node_name -- sh -c "apt update && apt upgrade -y"
-lxc exec $node_name -- sh -c "adduser --disabled-password --gecos '' $node_name && adduser $node_name sudo && echo '$node_name:$new_passwd' | chpasswd"
-lxc exec $node_name -- sh -c "usermod -s /usr/sbin/nologin root && usermod -s /usr/sbin/nologin ubuntu"
+lxc exec $node_name -- sh -c "adduser --disabled-password --gecos '' node && adduser node sudo && echo 'node:$new_passwd' | chpasswd"
+lxc exec $node_name -- sh -c "usermod -s /usr/sbin/nologin root"
+lxc exec $node_name -- sh -c "usermod -s /usr/sbin/nologin ubuntu"
 # lxc exec $node_name -- sh -c "sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config"
 
 lxc config set $node_name security.nesting true
 
 lxc restart $node_name
 
-echo "$node_name@$node_name : $new_passwd"
+echo "node@$node_name : $new_passwd"
 echo "Done."
