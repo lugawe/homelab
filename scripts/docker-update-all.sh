cbfaa3b087386440d11f7d2a3bc8dc71442fcf14
Luis Weich <luis.weich.de@gmail.com>
2024-03-23 15:26:34 +0100

    add docker-update-all.sh

---
+++ scripts/docker-update-all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+services=../docker
+
+server_files="$services/server"
+stats_files="$services/stats"
+vaultwarden_files="$services/vaultwarden"
+jellyfin_files="$services/jellyfin"
+gitea_files="$services/gitea"
+collabora_files="$services/collabora"
+nextcloud_files="$services/nextcloud"
+
+docker compose --file "$server_files/docker-compose.yml" --env-file "$server_files/.env" --project-name server pull
+docker compose --file "$stats_files/docker-compose.yml" --env-file "$stats_files/.env" --project-name stats pull
+docker compose --file "$vaultwarden_files/docker-compose.yml" --env-file "$vaultwarden_files/.env" --project-name vaultwarden pull
+docker compose --file "$jellyfin_files/docker-compose.yml" --env-file "$jellyfin_files/.env" --project-name jellyfin pull
+docker compose --file "$gitea_files/docker-compose.yml" --env-file "$gitea_files/.env" --project-name gitea pull
+docker compose --file "$collabora_files/docker-compose.yml" --env-file "$collabora_files/.env" --project-name collabora pull
+docker compose --file "$nextcloud_files/docker-compose.yml" --env-file "$nextcloud_files/.env" --project-name nextcloud pull
