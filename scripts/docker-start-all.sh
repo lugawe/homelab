0e761d8be5bd7feb05305a8573015b91d14e3b36
lugawe <luis.weich.de@gmail.com>
2024-05-08 00:09:54 +0200

    stats -> monitoring

--- scripts/docker-start-all.sh
+++ scripts/docker-start-all.sh
@@ -3,7 +3,7 @@
 services=../docker
 
 server_files="$services/server"
-stats_files="$services/stats"
+monitoring_files="$services/monitoring"
 vaultwarden_files="$services/vaultwarden"
 jellyfin_files="$services/jellyfin"
 gitea_files="$services/gitea"
@@ -11,7 +11,7 @@ collabora_files="$services/collabora"
 nextcloud_files="$services/nextcloud"
 
 docker compose --file "$server_files/docker-compose.yml" --env-file "$server_files/.env" --project-name server up -d
-docker compose --file "$stats_files/docker-compose.yml" --env-file "$stats_files/.env" --project-name stats up -d
+docker compose --file "$monitoring_files/docker-compose.yml" --env-file "$monitoring_files/.env" --project-name monitoring up -d
 docker compose --file "$vaultwarden_files/docker-compose.yml" --env-file "$vaultwarden_files/.env" --project-name vaultwarden up -d
 docker compose --file "$jellyfin_files/docker-compose.yml" --env-file "$jellyfin_files/.env" --project-name jellyfin up -d
 docker compose --file "$gitea_files/docker-compose.yml" --env-file "$gitea_files/.env" --project-name gitea up -d
--- scripts/docker-update-all.sh
+++ scripts/docker-update-all.sh
@@ -3,7 +3,7 @@
 services=../docker
 
 server_files="$services/server"
-stats_files="$services/stats"
+monitoring_files="$services/monitoring"
 vaultwarden_files="$services/vaultwarden"
 jellyfin_files="$services/jellyfin"
 gitea_files="$services/gitea"
@@ -11,7 +11,7 @@ collabora_files="$services/collabora"
 nextcloud_files="$services/nextcloud"
 
 docker compose --file "$server_files/docker-compose.yml" --env-file "$server_files/.env" --project-name server pull
-docker compose --file "$stats_files/docker-compose.yml" --env-file "$stats_files/.env" --project-name stats pull
+docker compose --file "$monitoring_files/docker-compose.yml" --env-file "$monitoring_files/.env" --project-name monitoring pull
 docker compose --file "$vaultwarden_files/docker-compose.yml" --env-file "$vaultwarden_files/.env" --project-name vaultwarden pull
 docker compose --file "$jellyfin_files/docker-compose.yml" --env-file "$jellyfin_files/.env" --project-name jellyfin pull
 docker compose --file "$gitea_files/docker-compose.yml" --env-file "$gitea_files/.env" --project-name gitea pull
