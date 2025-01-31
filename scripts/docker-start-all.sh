47645c3cf2ce0a8751235adf8eefa634cdb601f6
lugawe <luis.weich.de@gmail.com>
2025-01-31 02:46:46 +0100

    updates

--- scripts/docker-start-all.sh
+++ scripts/docker-start-all.sh
@@ -10,10 +10,12 @@ gitea_files="$services/gitea"
 collabora_files="$services/collabora"
 nextcloud_files="$services/nextcloud"
 
-docker compose --file "$server_files/docker-compose.yml" --env-file "$server_files/.env" --project-name server up -d
-docker compose --file "$monitoring_files/docker-compose.yml" --env-file "$monitoring_files/.env" --project-name monitoring up -d
-docker compose --file "$vaultwarden_files/docker-compose.yml" --env-file "$vaultwarden_files/.env" --project-name vaultwarden up -d
-docker compose --file "$jellyfin_files/docker-compose.yml" --env-file "$jellyfin_files/.env" --project-name jellyfin up -d
-docker compose --file "$gitea_files/docker-compose.yml" --env-file "$gitea_files/.env" --project-name gitea up -d
-docker compose --file "$collabora_files/docker-compose.yml" --env-file "$collabora_files/.env" --project-name collabora up -d
-docker compose --file "$nextcloud_files/docker-compose.yml" --env-file "$nextcloud_files/.env" --project-name nextcloud up -d
+env_file="../.env"
+
+docker compose --file "$server_files/docker-compose.yml" --env-file "$env_file" --project-name server up -d
+docker compose --file "$monitoring_files/docker-compose.yml" --env-file "$env_file" --project-name monitoring up -d
+docker compose --file "$vaultwarden_files/docker-compose.yml" --env-file "$env_file" --project-name vaultwarden up -d
+docker compose --file "$jellyfin_files/docker-compose.yml" --env-file "$env_file" --project-name jellyfin up -d
+docker compose --file "$gitea_files/docker-compose.yml" --env-file "$env_file" --project-name gitea up -d
+docker compose --file "$collabora_files/docker-compose.yml" --env-file "$env_file" --project-name collabora up -d
+docker compose --file "$nextcloud_files/docker-compose.yml" --env-file "$env_file" --project-name nextcloud up -d
--- scripts/docker-update-all.sh
+++ scripts/docker-update-all.sh
@@ -10,10 +10,12 @@ gitea_files="$services/gitea"
 collabora_files="$services/collabora"
 nextcloud_files="$services/nextcloud"
 
-docker compose --file "$server_files/docker-compose.yml" --env-file "$server_files/.env" --project-name server pull
-docker compose --file "$monitoring_files/docker-compose.yml" --env-file "$monitoring_files/.env" --project-name monitoring pull
-docker compose --file "$vaultwarden_files/docker-compose.yml" --env-file "$vaultwarden_files/.env" --project-name vaultwarden pull
-docker compose --file "$jellyfin_files/docker-compose.yml" --env-file "$jellyfin_files/.env" --project-name jellyfin pull
-docker compose --file "$gitea_files/docker-compose.yml" --env-file "$gitea_files/.env" --project-name gitea pull
-docker compose --file "$collabora_files/docker-compose.yml" --env-file "$collabora_files/.env" --project-name collabora pull
-docker compose --file "$nextcloud_files/docker-compose.yml" --env-file "$nextcloud_files/.env" --project-name nextcloud pull
+env_file="../.env"
+
+docker compose --file "$server_files/docker-compose.yml" --env-file "$env_file" --project-name server pull
+docker compose --file "$monitoring_files/docker-compose.yml" --env-file "$env_file" --project-name monitoring pull
+docker compose --file "$vaultwarden_files/docker-compose.yml" --env-file "$env_file" --project-name vaultwarden pull
+docker compose --file "$jellyfin_files/docker-compose.yml" --env-file "$env_file" --project-name jellyfin pull
+docker compose --file "$gitea_files/docker-compose.yml" --env-file "$env_file" --project-name gitea pull
+docker compose --file "$collabora_files/docker-compose.yml" --env-file "$env_file" --project-name collabora pull
+docker compose --file "$nextcloud_files/docker-compose.yml" --env-file "$env_file" --project-name nextcloud pull
