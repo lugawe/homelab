4881737ffb9f2a5c09b0d8d3bf3a023dbdea11ed
lugawe <luis.weich.de@gmail.com>
2025-02-23 22:49:55 +0100

    remove vaultwarden and collabora

--- scripts/docker-update-all.sh
+++ scripts/docker-update-all.sh
@@ -4,18 +4,14 @@ services=../services
 
 server_files="$services/server"
 monitoring_files="$services/monitoring"
-vaultwarden_files="$services/vaultwarden"
 jellyfin_files="$services/jellyfin"
 gitea_files="$services/gitea"
-collabora_files="$services/collabora"
 nextcloud_files="$services/nextcloud"
 
 env_file="$services/.env"
 
 docker compose --file "$server_files/compose.yml" --env-file "$env_file" --project-name server pull
 docker compose --file "$monitoring_files/compose.yml" --env-file "$env_file" --project-name monitoring pull
-docker compose --file "$vaultwarden_files/compose.yml" --env-file "$env_file" --project-name vaultwarden pull
 docker compose --file "$jellyfin_files/compose.yml" --env-file "$env_file" --project-name jellyfin pull
 docker compose --file "$gitea_files/compose.yml" --env-file "$env_file" --project-name gitea pull
-docker compose --file "$collabora_files/compose.yml" --env-file "$env_file" --project-name collabora pull
 docker compose --file "$nextcloud_files/compose.yml" --env-file "$env_file" --project-name nextcloud pull
