d894dddc1448433f97a87a1f7f8d32cd1cfe6f4e
lugawe <luis.weich.de@gmail.com>
2025-03-01 21:06:16 +0100

    updates

--- scripts/docker-start-all.sh
+++ scripts/docker-start-all.sh
@@ -4,14 +4,10 @@ services=../services
 
 server_files="$services/server"
 monitoring_files="$services/monitoring"
-jellyfin_files="$services/jellyfin"
-gitea_files="$services/gitea"
 nextcloud_files="$services/nextcloud"
 
 env_file="$services/.env"
 
 docker compose --file "$server_files/compose.yml" --env-file "$env_file" --project-name server up -d
 docker compose --file "$monitoring_files/compose.yml" --env-file "$env_file" --project-name monitoring up -d
-docker compose --file "$jellyfin_files/compose.yml" --env-file "$env_file" --project-name jellyfin up -d
-docker compose --file "$gitea_files/compose.yml" --env-file "$env_file" --project-name gitea up -d
 docker compose --file "$nextcloud_files/compose.yml" --env-file "$env_file" --project-name nextcloud up -d
--- scripts/docker-update-all.sh
+++ scripts/docker-update-all.sh
@@ -4,14 +4,10 @@ services=../services
 
 server_files="$services/server"
 monitoring_files="$services/monitoring"
-jellyfin_files="$services/jellyfin"
-gitea_files="$services/gitea"
 nextcloud_files="$services/nextcloud"
 
 env_file="$services/.env"
 
 docker compose --file "$server_files/compose.yml" --env-file "$env_file" --project-name server pull
 docker compose --file "$monitoring_files/compose.yml" --env-file "$env_file" --project-name monitoring pull
-docker compose --file "$jellyfin_files/compose.yml" --env-file "$env_file" --project-name jellyfin pull
-docker compose --file "$gitea_files/compose.yml" --env-file "$env_file" --project-name gitea pull
 docker compose --file "$nextcloud_files/compose.yml" --env-file "$env_file" --project-name nextcloud pull
