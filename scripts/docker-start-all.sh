f189c8dcc8ecb8c77c7444e7294a2ad77f03ca76
lugawe <luis.weich.de@gmail.com>
2025-03-13 23:02:22 +0100

    update

--- scripts/docker-start-all.sh
+++ scripts/docker-start-all.sh
@@ -5,9 +5,11 @@ services=../services
 server_files="$services/server"
 monitoring_files="$services/monitoring"
 nextcloud_files="$services/nextcloud"
+code_files="$services/code"
 
 env_file="$services/.env"
 
 docker compose --file "$server_files/compose.yml" --env-file "$env_file" --project-name server up -d
 docker compose --file "$monitoring_files/compose.yml" --env-file "$env_file" --project-name monitoring up -d
 docker compose --file "$nextcloud_files/compose.yml" --env-file "$env_file" --project-name nextcloud up -d
+docker compose --file "$code_files/compose.yml" --env-file "$env_file" --project-name code up -d
--- scripts/docker-update-all.sh
+++ scripts/docker-update-all.sh
@@ -5,9 +5,11 @@ services=../services
 server_files="$services/server"
 monitoring_files="$services/monitoring"
 nextcloud_files="$services/nextcloud"
+code_files="$services/code"
 
 env_file="$services/.env"
 
 docker compose --file "$server_files/compose.yml" --env-file "$env_file" --project-name server pull
 docker compose --file "$monitoring_files/compose.yml" --env-file "$env_file" --project-name monitoring pull
 docker compose --file "$nextcloud_files/compose.yml" --env-file "$env_file" --project-name nextcloud pull
+docker compose --file "$code_files/compose.yml" --env-file "$env_file" --project-name code up -d
