e2de696f4153259841b479ea799490dfa53fc57b
lugawe <luis.weich.de@gmail.com>
2025-03-13 23:16:22 +0100

    fix

--- scripts/docker-update-all.sh
+++ scripts/docker-update-all.sh
@@ -12,4 +12,4 @@ env_file="$services/.env"
 docker compose --file "$server_files/compose.yml" --env-file "$env_file" --project-name server pull
 docker compose --file "$monitoring_files/compose.yml" --env-file "$env_file" --project-name monitoring pull
 docker compose --file "$nextcloud_files/compose.yml" --env-file "$env_file" --project-name nextcloud pull
-docker compose --file "$code_files/compose.yml" --env-file "$env_file" --project-name code up -d
+docker compose --file "$code_files/compose.yml" --env-file "$env_file" --project-name code pull
