2a2f9e51e38ab00cd5e7370705602f4a7646b75d
Luis Weich <luis.gabriel.weich@gmail.com>
2025-10-22 17:24:51 +0200

    add jellyfin service

--- scripts/docker-update-all.sh
+++ scripts/docker-update-all.sh
@@ -5,6 +5,7 @@ services=../services
 server_files="$services/server"
 monitoring_files="$services/monitoring"
 nextcloud_files="$services/nextcloud"
+jellyfin_files="$services/jellyfin"
 code_files="$services/code"
 
 env_file="$services/.env"
@@ -12,4 +13,5 @@ env_file="$services/.env"
 docker compose --file "$server_files/compose.yml" --env-file "$env_file" --project-name server pull
 docker compose --file "$monitoring_files/compose.yml" --env-file "$env_file" --project-name monitoring pull
 docker compose --file "$nextcloud_files/compose.yml" --env-file "$env_file" --project-name nextcloud pull
+docker compose --file "$jellyfin_files/compose.yml" --env-file "$env_file" --project-name jellyfin pull
 docker compose --file "$code_files/compose.yml" --env-file "$env_file" --project-name code pull
